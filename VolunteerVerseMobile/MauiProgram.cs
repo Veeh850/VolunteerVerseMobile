5d89adcff56176f48983e7ca552c6ed7f9d059cd
Veress Henrietta <heniveress850@gmail.com>
2024-05-14 15:00:01 +0300

    Added Uranium UI library

--- VolunteerVerseMobile/MauiProgram.cs
+++ VolunteerVerseMobile/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using UraniumUI;
 using VolunteerVerseMobile.Interfaces;
 using VolunteerVerseMobile.Services;
 using VolunteerVerseMobile.ViewModels;
@@ -13,10 +14,13 @@ namespace VolunteerVerseMobile
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseUraniumUI()
+                .UseUraniumUIMaterial()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddMaterialIconFonts();
                 });
 
 #if DEBUG
